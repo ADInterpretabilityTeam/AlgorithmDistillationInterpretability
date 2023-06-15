549744d0365f1d318b0ea22e9d5555212f52bd90
victorLevoso <vlfernandez@esei.uvigo.es>
2023-06-15 20:17:03 +0200

    Fixed model index

--- src/streamlit_app/model_index.py
+++ src/streamlit_app/model_index.py
@@ -6,7 +6,7 @@ A dictionary of models users can choose between when using the app.
 model_index = {
     # The original post, reproduced with 1/10th of the total training epochs
     #"models/Test_09.pt": "test_paper_architecture",
-    "streamlit_app/model_folder/6l_darkroom.pt":"6l_darkroom",
+    "src/streamlit_app/model_folder/6l_darkroom.pt":"6l_darkroom",
     
 
 }
