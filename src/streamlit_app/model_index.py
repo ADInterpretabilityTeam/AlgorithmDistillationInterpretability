4b242d1a5bf98273b685d6a443a5d3db3b6907af
victorLevoso <vlfernandez@esei.uvigo.es>
2023-06-15 19:52:04 +0200

    Add model to the repo

---
+++ src/streamlit_app/model_index.py
@@ -0,0 +1,12 @@
+"""
+A dictionary of models users can choose between when using the app.
+
+"""
+
+model_index = {
+    # The original post, reproduced with 1/10th of the total training epochs
+    #"models/Test_09.pt": "test_paper_architecture",
+    "streamlit_app/models/6l_darkroom.pt":"6l_darkroom",
+    
+
+}
